7aaf67b9bc7f511653bfba79750dc3da161248aa
sarakhaghani <sskkmilani@gmail.com>
2023-08-21 20:30:24 +0330

    added axios integration

---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather() {
+  function handleResponse(response) {
+    alert(`the tempreture in Tehran is ${response.data.main.temp}`);
+  }
+
+  let url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=42ab2c2ee73f7e1a2ff24479d6688080&units=metric";
+
+  axios.get(url).then(handleResponse);
+
+  return <h2>hello from weather</h2>;
+}
